f70f881d6f9eee064e2eaaafacf62419aa3d85ff
Elic0de <55914415+Elic0de@users.noreply.github.com>
2024-01-22 10:01:12 +0900

    Update shiken.js

--- shiken.js
+++ shiken.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Kakomondojo
 // @namespace    http://tampermonkey.net/
 // @version      2024-01-19
 // @description  try to take over the world!
-// @author       You
+// @author       Eisei
 // @match        https://www.ap-siken.com/apkakomon.php
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @icon         https://www.ap-siken.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
@@ -13,5 +13,5 @@
     'use strict';
 
     // Your code here...
-    document.addEventListener("keypress",keypress_ivent);function keypress_ivent(a){"Enter"===a.code&&configform.submit();"Digit1"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(1) > button").click();"Digit2"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(2) > button").click();"Digit3"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(3) > button").click();"Digit4"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(4) > button").click();return!1};
+    document.addEventListener("keypress",keypress_ivent);function keypress_ivent(a){"Enter"===a.code&&configform.submit();"Space"===a.code&&configform.submit();"Digit1"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(1) > button").click();"Digit2"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(2) > button").click();"Digit3"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(3) > button").click();"Digit4"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(4) > button").click();return!1};
 })();
