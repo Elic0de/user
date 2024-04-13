93a779169d6369c6f432b4182980c824f0410c03
Elic0de <55914415+Elic0de@users.noreply.github.com>
2024-04-13 20:32:22 +0900

    Update shiken.js

--- shiken.js
+++ shiken.js
@@ -3,15 +3,25 @@
 // @namespace    http://tampermonkey.net/
 // @version      2024-01-19
 // @description  try to take over the world!
-// @author       Eisei
+// @author       Ei
 // @match        https://www.ap-siken.com/apkakomon.php
 // @icon         https://www.ap-siken.com/favicon.ico
+// @downloadURL  https://raw.githubusercontent.com/Elic0de/user/main/shiken.js
+// @updateURL    https://raw.githubusercontent.com/Elic0de/user/main/shiken.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
+    document.querySelectorAll("button[class=selectBtn]").forEach((array) => {
+    array.addEventListener("click", () => {
+      if (array?.id === "") {
+        new Audio('https://soundeffect-lab.info/sound/anime/mp3/incorrect1.mp3').play();
+        return;
+      }
+      new Audio('https://soundeffect-lab.info/sound/anime/mp3/correct2.mp3').play();
+    });
+  });
     // Your code here...
     document.addEventListener("keypress",keypress_ivent);function keypress_ivent(a){"Enter"===a.code&&configform.submit();"Space"===a.code&&configform.submit();"Digit1"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(1) > button").click();"Digit2"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(2) > button").click();"Digit3"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(3) > button").click();"Digit4"===a.code&&document.querySelector("#mainCol > div.main.kako.doujou > div.ansbg > ul > li:nth-child(4) > button").click();return!1};
 })();
